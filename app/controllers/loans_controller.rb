c4ec5f02c5f376a4fe835d5d5ccb240c12e1f34f
Rahul Verma <rahulv@gmail.com>
2014-07-12 13:43:00 +0530

    can display some data on page now
    views and controllers are still dummy. need to replace them with real once

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,5 +1,11 @@
 class LoansController < ApplicationController
   def index
+    @loans = Loan.all
+
+  end
+
+  def show
+    @loan = Loan.find params[:id]
 
   end
 end
--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,5 +1,11 @@
 class TradesController < ApplicationController
   def index
+    trades = Trade.all
+
+  end
+
+  def show
+    @trade = Trade.find params[:id]
 
   end
 end
